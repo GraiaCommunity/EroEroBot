7652bf6303d91e21f6d9649b603fdd577aaedc5b
Redlnn <w731347477@gmail.com>
2021-12-30 02:00:19 +0800

    根据文档更新并补充部署方法和README

---
+++ deploy.md
@@ -0,0 +1,124 @@
+## 如何部署 EroEroBot
+
+### 1. 环境要求
+
+虽然 `EroEroBot` 所依赖的 [`Ariadne`](https://github.com/GraiaProject/Ariadne) 框架目前要求的 Python 版本为 3.8 ~ 3.10
+
+但 `EroEroBot` 仍然建议你使用 Python 3.9 版本进行开发
+
+> 使用 Python 3.8 可能会存在一些未被发现的 Bug  
+> 而使用 Python 3.10 则可能会存在一些第三方库仍未支持的情况
+
+### 2. 配置 Mirai
+
+请参阅[Ariadne官方文档关于mah的配置方法](https://graia.readthedocs.io/appendix/mah-install/)
+
+### 3. 安装 Poetry
+
+> 注1：假设你不想用 `Poetry`，你可直接跳过  
+> 注2：此处假设你已在你的计算机或服务器上安装好 Python 3.9 / 3.10
+
+`EroEroBot` 使用 `Poetry` 来管理项目依赖关系
+
+#### Poetry 官方推荐的安装方法
+
+##### macOS / Linux / BashOnWindows
+
+```bash
+curl -sSL https://install.python-poetry.org | python3 -
+```
+
+##### Windows
+
+> 注：请使用 Windows 8 以上版本的 Windows，并通过 `Windows Powershell` 而不是 `Powershell Core` 执行下面的命令
+
+```powershell
+(Invoke-WebRequest -Uri https://install.python-poetry.org -UseBasicParsing).Content | python -
+```
+
+#### 通过 `pip` 来安装 `Poetry`
+
+假如你没有通过科学的姿势连接网络，上面的命令也许会超时失败  
+因此你也可以直接通过 `pip` 来安装 `Poetry`
+
+> 注：在 Windows 上使用 pip 时请不要设置系统代理，请通过设置镜像源来提升 pip 的下载速度
+
+##### macOS / Linux（此处假设你的 Python 3.9.x 版本的解释器是从源码通过 `make altinstall` 命令安装的）
+
+```bash
+python3.9 -m pip install poetry
+```
+
+##### Windows（请自行确保环境变量中的 Python 解释器版本符合要求）
+
+```powershell
+python -m pip install poetry
+```
+
+### 4. 克隆 EroEroBot 到本地并进入项目目录中
+
+```bash
+git clone https://github.com/Graiax-Community/EroEroBot.git
+cd EroEroBot
+```
+
+### 5. 创建虚拟环境并安装依赖
+
+> 此处假设你所使用的 Python 版本为 3.9
+
+```bash
+poetry env use 3.9
+poetry install
+```
+
+### 6. 启动 EroEroBot
+
+请不要将以下命令写入 `*.sh`、`*.bat`、`*.cmd`、`*.ps1` 等脚本中使用
+
+```bash
+poetry run python main.py
+```
+
+## 让 EroEroBot 保持在后台运行
+
+### Windows / macOS
+
+~~不会吧不会吧，不会有人连最小化都不会吧~~
+
+## Linux
+
+### 安装 screen
+
+#### CentOS 等 RPM 系
+
+```bash
+sudo yum install repl-release
+sudo yum install screen
+```
+
+#### Ubuntu 等 dpkg 系
+
+```bash
+sudo apt update
+sudo apt install screen
+```
+
+### 使用 screen
+
+1. 创建一个 screen
+
+   ```bash
+   screen -S eroerobot
+   ```
+
+2. 将 screen 放到后台
+
+  > 在 screen 内先按下 `ctrl + a` 组合键，再按下 `d` 键
+
+3. 将后台的 screen 调出来
+
+   ```bash
+   screen -x eroerobot  # 回到名为 eroerobot 的 screen
+   screen -x  # 回到最近用过的 screen
+   screen -r  # 回到最近用过的 screen
+   ```
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import os
 import random
 from datetime import datetime
 
+import aiohttp
 from graia.ariadne.app import Ariadne
+from graia.ariadne.context import adapter_ctx
 from graia.ariadne.event.message import GroupMessage
 from graia.ariadne.event.mirai import NudgeEvent
 from graia.ariadne.message.chain import MessageChain
@@ -30,13 +32,13 @@ app = Ariadne(
 bcc = app.broadcast
 
 
-# 1-3.快速创建一个最小实例
+# 1-3. 快速创建一个最小实例
 @bcc.receiver(GroupMessage)
 async def setu(app: Ariadne, group: Group, message: MessageChain):
     await app.sendGroupMessage(group, MessageChain.create(f"不要说{message.asDisplay()}，来点涩图"))
 
 
-# 2.不要再戳了~
+# 2. 不要再戳了~
 @bcc.receiver(NudgeEvent)
 async def getup(app: Ariadne, event: NudgeEvent):
     if event.context_type == "group":
@@ -45,22 +47,22 @@ async def getup(app: Ariadne, event: NudgeEvent):
         await app.sendFriendMessage(event.friend_id, MessageChain.create("别戳我，好痒"))
 
 
-# 3-4.Twilight的简单运用
+# 3-4. Twilight的简单运用
 @bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("涩图来")]))])
 async def test(app: Ariadne, group: Group):
     await app.sendGroupMessage(group, MessageChain.create(Image(path=os.path.join("imgs", "graiax.png"))))
 
 
-# 4.关于发送语音的一些小问题
+# 4. 关于发送语音的一些小问题
 @bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("语音")]))])
 async def hentai(app: Ariadne, group: Group):
     audio_bytes = await silkcoder.encode(os.path.join("voices", "hentai.mp3"))
     await app.sendGroupMessage(group, MessageChain.create(Voice(data_bytes=audio_bytes)))
 
 
-# 5.好大的奶
-#   > 你可以自己无中生有生成消息链然后传播出去
-#   > 请不要通过该方法传播谣言
+# 5. 好大的奶
+#    > 你可以自己无中生有生成消息链然后传播出去
+#    > 请不要通过该方法传播谣言
 @bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("好大的奶")]))])
 async def create_forward(app: Ariadne, group: Group, member: Member):
     forward_nodes = [
@@ -84,6 +86,15 @@ async def create_forward(app: Ariadne, group: Group, member: Member):
         )
     message = MessageChain.create(Forward(nodeList=forward_nodes))
     await app.sendGroupMessage(group, message)
+
+
+# 6. 来点网上的涩图 -- 直接使用Ariadne自带的session进行请求
+@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("来点网上的涩图2")]))])
+async def download_img2(app: Ariadne, group: Group):
+    session = adapter_ctx.get().session
+    async with session.get("https://api.ixiaowai.cn/api/api.php") as resp:
+        img_bytes = await resp.read()
+    await app.sendGroupMessage(group, MessageChain.create(Image(data_bytes=img_bytes)))
 
 
 app.launch_blocking()
