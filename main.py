c3b086cc0e616278625c74ec5ba23551796cf128
Redlnn <w731347477@gmail.com>
2022-01-30 13:29:46 +0800

    跟随文档更新——11章定时消息 & 12章使用Saya进行模块化

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
-import os
+import pkgutil
 import random
 from datetime import datetime
+from os.path import abspath, dirname, join
 
 import aiohttp
 from graia.ariadne.app import Ariadne
@@ -26,6 +27,8 @@ from graia.ariadne.message.parser.twilight import FullMatch, Sparkle, Twilight
 from graia.ariadne.model import Group, Member, MiraiSession
 from graia.broadcast.builtin.decorators import Depend
 from graia.broadcast.exceptions import ExecutionStop
+from graia.scheduler import GraiaScheduler, timers
+from graia.scheduler.saya import GraiaSchedulerBehaviour
 from graiax import silkcoder
 
 app = Ariadne(
@@ -37,18 +40,50 @@ app = Ariadne(
     ),
 )
 
-bcc = app.broadcast
+ROOT_PATH = abspath(dirname(__file__))  # 机器人根目录
 
+bcc = app.broadcast
+loop = app.loop
+sche = GraiaScheduler(loop=loop, broadcast=bcc)
+
+
+# 从第 12 章起，将使用 graia-Saya 作为插件加载框架
+# 即将不同的功能分为不同的文件（模块化）
+# 因此，此部分为 Saya 的初始化以及模块的加载代码
+# 第 1 到 11 章部分的代码将放在此部分之后
+# 在本例中，各种模块将放置于 {机器人根目录}/plugins/ 中
+# 在本例中，各个模块按章节而不是按功能分开
+# Saya Initialization - Start
+# --------------------------------------------
+from graia.saya import Saya
+from graia.saya.builtins.broadcast import BroadcastBehaviour
+
+saya = Saya(bcc)  # 初始化 Saya
+saya.install_behaviours(BroadcastBehaviour(bcc))  # 为 bcc 安装 BroadcastBehaviour
+saya.install_behaviours(GraiaSchedulerBehaviour(sche))  # 为定时任务安装 GraiaSchedulerBehaviour
+# --------------------------------------------
+# Saya Initialization - End
+# Saya Module Load - Start
+# --------------------------------------------
+with saya.module_context():
+    for module in pkgutil.iter_modules([join(ROOT_PATH, "modules")]):
+        saya.require(f"modules.{module.name}")
+# --------------------------------------------
+# Saya Module Load - End
+
+# 第 1 到 11 章部分的代码从此处开始
+# ========================================================================================
 
 # 1-3. 快速创建一个最小实例
-@bcc.receiver(GroupMessage)
-async def setu(app: Ariadne, group: Group, message: MessageChain):
-    await app.sendGroupMessage(group, MessageChain.create(f"不要说{message.asDisplay()}，来点涩图"))
+# 由于每收到一条群消息都会触发，为防止风控及骚扰，默认注释
+# @bcc.receiver(GroupMessage)
+# async def min_test(app: Ariadne, group: Group, message: MessageChain):
+#     await app.sendGroupMessage(group, MessageChain.create(f"不要说{message.asDisplay()}，来点涩图"))
 
 
 # 2. 不要再戳了~
 @bcc.receiver(NudgeEvent)
-async def getup(app: Ariadne, event: NudgeEvent):
+async def nudge_test(app: Ariadne, event: NudgeEvent):
     if event.context_type == "group":
         await app.sendGroupMessage(event.group_id, MessageChain.create("别戳我，好痒"))
     else:
@@ -56,28 +91,28 @@ async def getup(app: Ariadne, event: NudgeEvent):
 
 
 # 3-4. Twilight的简单运用
-@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("涩图来")]))])
-async def test(app: Ariadne, group: Group):
-    await app.sendGroupMessage(group, MessageChain.create(Image(path=os.path.join("imgs", "graiax.png"))))
+@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("3-4涩图来")]))])
+async def twilight_test(app: Ariadne, group: Group):
+    await app.sendGroupMessage(group, MessageChain.create(Image(path=join(ROOT_PATH, "data", "imgs", "graiax.png"))))
 
 
 # 4. 关于发送语音的一些小问题
-@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("语音")]))])
-async def hentai(app: Ariadne, group: Group):
-    audio_bytes = await silkcoder.encode(os.path.join("voices", "hentai.mp3"))
+@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("4语音")]))])
+async def voice_test(app: Ariadne, group: Group):
+    audio_bytes = await silkcoder.encode(join(ROOT_PATH, "data", "voices", "hentai.mp3"))
     await app.sendGroupMessage(group, MessageChain.create(Voice(data_bytes=audio_bytes)))
 
 
 # 5. 好大的奶
 #    > 你可以自己无中生有生成消息链然后传播出去
 #    > 请不要通过该方法传播谣言
-@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("好大的奶")]))])
-async def create_forward(app: Ariadne, group: Group, member: Member):
+@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("5好大的奶")]))])
+async def forward_test(app: Ariadne, group: Group, member: Member):
     forward_nodes = [
         ForwardNode(
             target=member,  # 发送者的信息(Member / Friend / Stranger 都行)
             time=datetime.now(),  # 发送时间
-            message=MessageChain.create(Image(path=os.path.join("imgs", "huge_milk.jpg"))),  # 要发送的消息链
+            message=MessageChain.create(Image(path=join(ROOT_PATH, "data", "imgs", "huge_milk.jpg"))),  # 要发送的消息链
         )
     ]
     member_list = await app.getMemberList(group)
@@ -95,8 +130,8 @@ async def create_forward(app: Ariadne, group: Group, member: Member):
 
 
 # 6. 来点网上的涩图 -- 直接使用Ariadne自带的session进行请求
-@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("来点网上的涩图2")]))])
-async def download_img2(app: Ariadne, group: Group):
+@bcc.receiver(GroupMessage, dispatchers=[Twilight(Sparkle([FullMatch("6来点网上的涩图")]))])
+async def download_test(app: Ariadne, group: Group):
     session = adapter_ctx.get().session
     async with session.get("https://i1.hdslb.com/bfs/archive/5242750857121e05146d5d5b13a47a2a6dd36e98.jpg") as resp:
         img_bytes = await resp.read()
@@ -104,8 +139,8 @@ async def download_img2(app: Ariadne, group: Group):
 
 
 # 8. 看完了吗，我撤回了
-@bcc.receiver(GroupMessage, dispatchers=[Twilight.from_command("撤回")])
-async def test(app: Ariadne, group: Group, source: Source):
+@bcc.receiver(GroupMessage, dispatchers=[Twilight.from_command("8撤回")])
+async def recall_test(app: Ariadne, group: Group, source: Source):
     session = adapter_ctx.get().session
     async with session.get("https://i1.hdslb.com/bfs/archive/5242750857121e05146d5d5b13a47a2a6dd36e98.jpg") as resp:
         data = await resp.read()
@@ -118,8 +153,8 @@ async def test(app: Ariadne, group: Group, source: Source):
 
 
 # 9. /斜眼笑
-@bcc.receiver(GroupMessage, dispatchers=[Twilight.from_command("斜眼笑")])
-async def test(app: Ariadne, group: Group):
+@bcc.receiver(GroupMessage, dispatchers=[Twilight.from_command("9斜眼笑")])
+async def format_test(app: Ariadne, group: Group):
     # await app.sendGroupMessage(
     #     group,
     #     MessageChain.create(
@@ -144,11 +179,11 @@ async def test(app: Ariadne, group: Group):
 
 
 # 10. 不是所有人都能看涩图 - Start
-# ----------------------------------------------
+# --------------------------------------------
 # 本示例中部分代码并不能真正发挥作用，仅保证bot可以运行
 # 请勿在未修改的情况下触发，若因触发该部分代码而出现错误
 # 请不要发送 issue 或在其他平台询问
-# ----------------------------------------------
+# --------------------------------------------
 def check_group(*groups: int):
     async def check_group_deco(app: Ariadne, group: Group):
         if group.id not in groups:
@@ -187,9 +222,9 @@ def check_frequency(max_frequency: int):
         check_member(114514, 1919810),
         check_frequency(10),
     ],
-    dispatchers=[Twilight.from_command("Depend测试")],
+    dispatchers=[Twilight.from_command("10Depend测试")],
 )
-async def setu(app: Ariadne, group: Group, member: Member):
+async def check_test(app: Ariadne, group: Group, member: Member):
     # 此处的链接仅为示例，用于获取涩图api的限制，实际并不存在，请自行替换
     async with aiohttp.request("GET", "https://setu.example.com/limit") as r:
         is_max = (await r.json())["is_limit"]
@@ -200,7 +235,22 @@ async def setu(app: Ariadne, group: Group, member: Member):
         # await app.sendGroupMessage(group, MessageChain.create(Image(data_bytes=setu)))
 
 
-# ----------------------------------------------
+# --------------------------------------------
 # 10. 不是所有人都能看涩图 - End
+
+# 11-1. 哦嗨哟，欧尼酱 —— 每分钟都在群里发垃圾消息
+# 为避免风控及骚扰，此部分代码默认注释掉
+# @sche.schedule(timers.every_minute())
+# async def every_minute_speaki_test(app: Ariadne):
+#     await app.sendGroupMessage((await app.getGroupList())[0], MessageChain.create("我又来了"))
+
+# 11-2. 哦嗨哟，欧尼酱 —— 每天7点30分30秒叫起床
+@sche.schedule(timers.crontabify("30 7 * * * 30"))
+async def every_minute_speaki_test(app: Ariadne):
+    await app.sendGroupMessage((await app.getGroupList())[0], MessageChain.create("哦嗨哟，欧尼酱"))
+
+
+# ========================================================================================
+# 第 1 到 11 章部分的代码到此结束
 
 app.launch_blocking()
---
+++ modules/12_ero.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+第12章 东西要分类好
+"""
+
+from graia.ariadne.app import Ariadne
+from graia.ariadne.event.message import GroupMessage
+from graia.ariadne.message.chain import MessageChain
+from graia.ariadne.model import Group
+from graia.saya import Channel
+from graia.saya.builtins.broadcast.schema import ListenerSchema
+from graia.scheduler import timers
+from graia.scheduler.saya import SchedulerSchema
+
+channel = Channel.current()
+
+channel.name("Ero | Wyy")
+channel.description("发送涩涩！ | 网易云时间提醒器")
+channel.author("GraiaX")
+
+
+@channel.use(ListenerSchema(listening_events=[GroupMessage]))
+async def ero(app: Ariadne, group: Group, message: MessageChain):
+    await app.sendGroupMessage(group, MessageChain.create(f"不要说{message.asDisplay()}，来点涩图"))
+
+
+@channel.use(SchedulerSchema(timers.crontabify("0 0 * * *")))
+async def wyy(app: Ariadne):
+    await app.sendGroupMessage(1919810, MessageChain.create("现在是网易云时间"))
