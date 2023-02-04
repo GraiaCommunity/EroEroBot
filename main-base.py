78c6d5ab0aeb03988fd6afedef1d218f23935329
Redlnn <w731347477@gmail.com>
2023-02-04 23:15:41 +0800

    update

--- main-base.py
+++ main-base.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import asyncio
-
 from creart import create
 from graia.ariadne.app import Ariadne
 from graia.ariadne.connection.config import (
@@ -20,7 +18,7 @@ app = Ariadne(
     connection=config(
         1919810,  # 你的机器人的 qq 号
         "GraiaxVerifyKey",  # 填入你的 mirai-api-http 配置中的 verifyKey
-        # 以下两行（不含注释）里的 host 参数的地址
+        # 以下两行代码（不含注释）里的 host 参数的地址
         # 是你的 mirai-api-http 地址中的地址与端口
         # 他们默认为 "http://localhost:8080"
         # 如果你 mirai-api-http 的地址与端口也是 localhost:8080
